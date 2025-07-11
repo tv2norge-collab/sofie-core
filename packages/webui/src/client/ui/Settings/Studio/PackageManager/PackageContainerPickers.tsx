04e8cca5afa2dfc818bdbd4a6e0f50f163631ed8
ianshade <ianshade@users.noreply.github.com>
2025-07-11 22:53:07 +0200

    fix(EAV-663) package ids multiselects styling and behavior

--- packages/webui/src/client/ui/Settings/Studio/PackageManager/PackageContainerPickers.tsx
+++ packages/webui/src/client/ui/Settings/Studio/PackageManager/PackageContainerPickers.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
-import { DBStudio, StudioPackageContainer } from '@sofie-automation/corelib/dist/dataModel/Studio'
-import { EditAttribute } from '../../../../lib/EditAttribute'
+import { DBStudio, StudioContainerIds, StudioPackageContainer } from '@sofie-automation/corelib/dist/dataModel/Studio'
 import { useTranslation } from 'react-i18next'
 import { Accessor } from '@sofie-automation/blueprints-integration'
 import { Studios } from '../../../../collections'
 import { DropdownInputOption } from '../../../../lib/Components/DropdownInput'
-import { WrappedOverridableItem } from '../../util/OverrideOpHelper'
+import { useOverrideOpHelper, WrappedOverridableItem, WrappedOverridableItemNormal } from '../../util/OverrideOpHelper'
+import { LabelAndOverridesForMultiSelect } from '../../../../lib/Components/LabelAndOverrides'
+import {
+	applyAndValidateOverrides,
+	ObjectWithOverrides,
+	SomeObjectOverrideOp,
+} from '@sofie-automation/corelib/dist/settings/objectWithOverrides'
+import { MultiSelectInputControl } from '../../../../lib/Components/MultiSelectInput'
+import { useMemo } from 'react'
 
 interface PackageContainersPickersProps {
 	studio: DBStudio
@@ -17,6 +24,47 @@ export function PackageContainersPickers({
 	packageContainersFromOverrides,
 }: PackageContainersPickersProps): JSX.Element {
 	const { t } = useTranslation()
+
+	const [wrappedItem, wrappedConfigObject] = useMemo(() => {
+		const prefixedOps = studio.packageContainerIdsWithOverrides.overrides.map((op) => ({
+			...op,
+			// TODO: can we avoid doing this hack?
+			path: `0.${op.path}`,
+		}))
+
+		const computedValue = applyAndValidateOverrides(studio.packageContainerIdsWithOverrides).obj
+
+		const wrappedItem: WrappedOverridableItemNormal<StudioContainerIds> = {
+			type: 'normal',
+			id: '0',
+			computed: computedValue,
+			defaults: studio.packageContainerIdsWithOverrides.defaults,
+			overrideOps: prefixedOps,
+		}
+
+		const wrappedConfigObject: ObjectWithOverrides<StudioContainerIds> = {
+			defaults: studio.packageContainerIdsWithOverrides.defaults,
+			overrides: prefixedOps,
+		}
+
+		return [wrappedItem, wrappedConfigObject]
+	}, [studio.packageContainerIdsWithOverrides])
+
+	const saveOverrides = React.useCallback(
+		(newOps: SomeObjectOverrideOp[]) => {
+			console.log('updating studio', newOps)
+			Studios.update(studio._id, {
+				$set: {
+					'packageContainerIdsWithOverrides.overrides': newOps.map((op) => ({
+						...op,
+						path: op.path.startsWith('0.') ? op.path.slice(2) : op.path,
+					})),
+				},
+			})
+		},
+		[studio._id]
+	)
+	const overrideHelper = useOverrideOpHelper(saveOverrides, wrappedConfigObject)
 
 	const availablePackageContainerOptions = React.useMemo(() => {
 		const arr: DropdownInputOption<string>[] = []
@@ -43,33 +91,41 @@ export function PackageContainersPickers({
 	}, [packageContainersFromOverrides])
 
 	return (
-		<div>
-			<div className="field mvs">
-				<label>{t('Package Containers to use for previews')}</label>
-				<div className="mdi">
-					<EditAttribute
-						attribute="previewContainerIds"
-						obj={studio}
-						options={availablePackageContainerOptions}
-						label={t('Click to show available Package Containers')}
-						type="multiselect"
-						collection={Studios}
+		<div className="properties-grid">
+			<LabelAndOverridesForMultiSelect
+				label={t('Package Containers to use for previews')}
+				hint={t('Click to show available Package Containers')}
+				item={wrappedItem}
+				itemKey={'previewContainerIds'}
+				overrideHelper={overrideHelper}
+				options={availablePackageContainerOptions}
+			>
+				{(value, handleUpdate, options) => (
+					<MultiSelectInputControl
+						classNames="input input-l"
+						options={options}
+						value={value}
+						handleUpdate={handleUpdate}
 					/>
-				</div>
-			</div>
-			<div className="field mvs">
-				<label>{t('Package Containers to use for thumbnails')}</label>
-				<div className="mdi">
-					<EditAttribute
-						attribute="thumbnailContainerIds"
-						obj={studio}
-						options={availablePackageContainerOptions}
-						label={t('Click to show available Package Containers')}
-						type="multiselect"
-						collection={Studios}
+				)}
+			</LabelAndOverridesForMultiSelect>
+			<LabelAndOverridesForMultiSelect
+				label={t('Package Containers to use for thumbnails')}
+				hint={t('Click to show available Package Containers')}
+				item={wrappedItem}
+				itemKey={'thumbnailContainerIds'}
+				overrideHelper={overrideHelper}
+				options={availablePackageContainerOptions}
+			>
+				{(value, handleUpdate, options) => (
+					<MultiSelectInputControl
+						classNames="input input-l"
+						options={options}
+						value={value}
+						handleUpdate={handleUpdate}
 					/>
-				</div>
-			</div>
+				)}
+			</LabelAndOverridesForMultiSelect>
 		</div>
 	)
 }
